547b031dd9d67d118d319faaab4da10f299beb82
Movj <xpwolf1999@gmail.com>
2019-09-04 13:33:38 -0500

    Preparing for data sorting filter and pagination

---
+++ CE.API/Helpers/ResourceParameters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CE.API.Helpers
+{
+    public class ResourceParameters
+    {
+        const int maxPageSize = 20;
+        public int pageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
+        public string SearchQuery { get; set; }
+        public string OrderBy { get; set; } = "Name";
+    }
+}
